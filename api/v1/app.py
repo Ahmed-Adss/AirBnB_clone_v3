fd4934712b9c5ad554924e2224260d2b5d767341
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-29 15:11:28 +0200

    commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -32,11 +32,11 @@ def not_found(error):
 
     '''
 
-    response = {"error" : "Not found"}
+    response = {"error": "Not found"}
     return jsonify(response), 404
 
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(debug=True, host=HOST, port=PORT,threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
